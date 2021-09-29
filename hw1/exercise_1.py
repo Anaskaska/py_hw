a0e842651d8618a68fb773e4cecf095fbb1e7841
Anaskaska <avmakarova@ya.ru>
2021-09-29 07:22:12 +0300

    add hw1

---
+++ hw1/exercise_1.py
@@ -0,0 +1,14 @@
+a = 3
+b = a + 5
+print("3 + 5 =", b)
+c = a % b
+print ("3 % 5 =", c)
+
+d = float(input ("Input a number 1 "))
+e = float(input ("Input a number 2 "))
+print("Your numbers are", d," and ", e)
+print ("number 1 + number 2 = ", d + e)
+
+name = input("input the name")
+friends_name = input("input the name of your friend ")
+print ("your name is", name, "your friend's name is", friends_name)
